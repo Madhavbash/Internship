c13af545f05b549d9b78e7240b05400e86121dab
Your Name <madhav.jc@gmail.com>
2021-06-02 13:43:43 +0200

    Plot and initial logit for fruit scores

--- Fruit_score analysis.R
+++ Fruit_score analysis.R
@@ -1,9 +1,22 @@
 ### fruit score analysis
 library(readxl)
+library(ggplot2)
+library(ggbeeswarm)
 library(MASS)
 file3 <- "Data/Symptoms_fruits.xlsx"
 scores <- read_excel(file3)
 str(scores)
 scores <- data.frame(scores)
+scores$symptom4week <- as.factor(scores$symptom4week)
+scores$symptom7week <- as.factor(scores$symptom7week)
+scores$symptom9week <- as.factor(scores$symptom9week)
+
+##plots
+ ggplot(scores, aes(x = treatment, fill = symptom9week))+
+   geom_bar(position = "stack")+
+   facet_grid(inoculation ~ puncture, margins = FALSE)
+#NOTE: inoculation iss vertically along the grid and puncture is horizontally.
 
 ##Ordinal logit model
+m1 <- polr(symptom7week ~ treatment + puncture + inoculation, data = scores)
+summary(m1)
