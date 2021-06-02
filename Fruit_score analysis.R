26309eb0f87c02cfd31eb47fb228682a44069f93
Your Name <madhav.jc@gmail.com>
2021-06-02 12:50:09 +0200

    Changes in the excel sheet

--- Fruit_score analysis.R
+++ Fruit_score analysis.R
@@ -1,2 +1,9 @@
 ### fruit score analysis
 library(readxl)
+library(MASS)
+file3 <- "Data/Symptoms_fruits.xlsx"
+scores <- read_excel(file3)
+str(scores)
+scores <- data.frame(scores)
+
+##Ordinal logit model
